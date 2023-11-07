a0b5eb5ae38deb48b72da7e8c9dd57152a1447d6
Vignesh <55778754+Vignesh-VIT@users.noreply.github.com>
2023-11-07 19:16:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,13 +3,13 @@ import sys
 label = sys.argv[1]
 
 # Read the RST file
-with open('path/to/your/file.rst', 'r') as file:
+with open('home/runner/work/Traceability/Traceability/*.rst', 'r') as file:
     rst_content = file.read()
 
 # Update a specific placeholder with the label value
-placeholder = "Label Placeholder: "
+placeholder = "..Version: "
 new_rst_content = rst_content.replace(placeholder, f"{placeholder}{label}")
 
 # Write the updated content back to the RST file
-with open('path/to/your/file.rst', 'w') as file:
+with open('home/runner/work/Traceability/Traceability/*.rst', 'w') as file:
     file.write(new_rst_content)
